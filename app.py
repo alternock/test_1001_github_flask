37c0e55229f5958edbf6afc65250bd90ee0e14eb
alternock <montilla269@gmail.com>
2026-03-30 08:55:11 +0200

    add new endpoint /users

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +8,11 @@ CORS(app)
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "ok"})
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    return jsonify(response.json())
 
 
 if __name__ == "__main__":
